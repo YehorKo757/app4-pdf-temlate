ce829b693dcea7ca2a2b1771ec4ebc45c6e29820
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-08-15 13:16:54 +0100

    Day24: Add lines

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ for index, row in data.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1, border=0)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+    for y in range(21,298,10):
+        pdf.line(x1=10, y1=y, x2=200, y2=y)
 
     # Set the footer for master page
-    pdf.ln(265)
+    pdf.ln(267)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R",
@@ -23,8 +24,10 @@ for index, row in data.iterrows():
 
     for i in range(0,row["Pages"]-1):
         pdf.add_page()
+        for y in range(21, 298, 10):
+            pdf.line(x1=10, y1=y, x2=200, y2=y)
         # Set the footer for other pages
-        pdf.ln(277)
+        pdf.ln(279)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R",
